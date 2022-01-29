c85b8895e8a68edb329aaa0072b10c2f970be887
A. Pushpendra <pushpendra@ncompass.in>
2022-01-29 16:07:13 +0530

    Added Timer and Date Selector

--- app/src/main/java/com/example/todoapp/Screens/AddTodoFragment.kt
+++ app/src/main/java/com/example/todoapp/Screens/AddTodoFragment.kt
@@ -1,10 +1,13 @@
 package com.example.todoapp.Screens
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
 import androidx.navigation.fragment.findNavController
 import com.example.todoapp.databinding.FragmentAddTodoBinding
 
@@ -13,6 +16,8 @@ import com.example.todoapp.databinding.FragmentAddTodoBinding
 class AddTodoFragment : Fragment() {
 
     lateinit var binding : FragmentAddTodoBinding
+    var dateStr = ""
+    var timeStr = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,11 +37,42 @@ class AddTodoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.fabAddTodoTrue.setOnClickListener {
 
+        binding.fabAddTodoTrue.setOnClickListener {
+            with(binding){
+                if(!tvSelectDate.text.toString().contains(":") || !tvSelectTime.text.toString().contains(":") || etTodoText.text.toString().isEmpty())
+                {
+                    Toast.makeText(context, "All Fields are not Set. Set the Fields and Try Again", Toast.LENGTH_SHORT).show()
+                    return@setOnClickListener
+                } else {
+                    // DB Insert
+                }
+            }
         }
         binding.fabTodoCancel.setOnClickListener {
             findNavController().popBackStack()
+        }
+
+        binding.datePicker.setOnDateChangedListener { view, year, monthOfYear, dayOfMonth ->
+            dateStr = year.toString()  + "/" + monthOfYear.toString() + "/" + dayOfMonth.toString()
+            binding.tvSelectDate.text = "Selected Date: " + dateStr
+
+        }
+
+        binding.timePicker.setIs24HourView(true)
+        binding.timePicker.setOnTimeChangedListener { view, hourOfDay, minute ->
+            timeStr = hourOfDay.toString() + ":" + minute.toString()
+            binding.tvSelectTime.text = "Selected Time: " + timeStr
+        }
+
+        with(binding.datePicker){
+            dateStr = year.toString()  + "/" + month.toString() + "/" + dayOfMonth.toString()
+            binding.tvSelectDate.text = "Selected Date: " + dateStr
+        }
+
+        with(binding.timePicker){
+            timeStr = hour.toString() + ":" + minute.toString()
+            binding.tvSelectTime.text = "Selected Time: " + timeStr
         }
     }
 
